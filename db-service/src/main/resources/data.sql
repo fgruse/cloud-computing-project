edf5d6fba9465a825a66186a337c63efac8b8369
Sophie Schröder <sophieschroder@sophies-mbp.speedport.ip>
2020-01-06 16:02:09 +0100

    db

--- db-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO c_user (id, first_name, last_name) VALUES (1, 'foo', 'bar');
-INSERT INTO c_user (id, first_name, last_name) VALUES (2, 'jon', 'smith');
-INSERT INTO c_user (id, first_name, last_name) VALUES (3, 'mariah', 'musterfrau');
--- db-service/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS C_USER;
-CREATE TABLE C_USER
-(
-    id        INTEGER PRIMARY KEY auto_increment,
-    first_name VARCHAR(64),
-    last_name  VARCHAR(64)
-);
