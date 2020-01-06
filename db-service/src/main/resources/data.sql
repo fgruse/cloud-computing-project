c93ad4a755cb1c2dd0144c5ae085a5c8f165a22f
Anna-Frieda Gruse <anna-frieda.gruse@zalando.de>
2020-01-06 14:07:04 +0100

    wip db service

---
+++ db-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO c_user (id, first_name, last_name) VALUES (1, 'foo', 'bar');
+INSERT INTO c_user (id, first_name, last_name) VALUES (2, 'jon', 'smith');
+INSERT INTO c_user (id, first_name, last_name) VALUES (3, 'mariah', 'musterfrau');
