b417d0b717c4ec43a2288b1296379d9c2c4a58b8
Anna-Frieda Gruse <anna-frieda.gruse@zalando.de>
2020-02-11 16:01:12 +0100

    connect to db-service api

--- ui-service/src/main/java/com/github/cosmoem/uiservice/handlingformsubmission/Flug.java
+++ ui-service/src/main/java/com/github/cosmoem/uiservice/handlingformsubmission/Flug.java
@@ -1,24 +1,99 @@
 package com.github.cosmoem.uiservice.handlingformsubmission;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Flug {
+
+    private int id;
     private String flugnummer;
-    private String content;
+    private String airline;
+    private String von;
+    private String nach;
+    private Date flugdatum;
+    private LocalTime uhrzeit;
+
+    public Flug(final int id, final String flugnummer, final String airline, final String von, final String nach, final Date flugdatum,
+            final LocalTime uhrzeit) {
+        this.id = id;
+        this.flugnummer = flugnummer;
+        this.airline = airline;
+        this.von = von;
+        this.nach = nach;
+        this.flugdatum = flugdatum;
+        this.uhrzeit = uhrzeit;
+    }
+
+    public Flug() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
 
     public String getFlugnummer() {
         return flugnummer;
     }
 
-    public void setFlugnummer(String flugnummer) {
+    public void setFlugnummer(final String flugnummer) {
         this.flugnummer = flugnummer;
     }
 
-
-    public String getContent() {
-        return content;
+    public String getAirline() {
+        return airline;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setAirline(final String airline) {
+        this.airline = airline;
     }
 
+    public String getVon() {
+        return von;
+    }
+
+    public void setVon(final String von) {
+        this.von = von;
+    }
+
+    public String getNach() {
+        return nach;
+    }
+
+    public void setNach(final String nach) {
+        this.nach = nach;
+    }
+
+    public Date getFlugdatum() {
+        return flugdatum;
+    }
+
+    public void setFlugdatum(final Date flugdatum) {
+        this.flugdatum = flugdatum;
+    }
+
+    public LocalTime getUhrzeit() {
+        return uhrzeit;
+    }
+
+    public void setUhrzeit(final LocalTime uhrzeit) {
+        this.uhrzeit = uhrzeit;
+    }
+
+    @Override
+    public String toString() {
+        return "Flug{" +
+                "id=" + id +
+                ", flugnummer='" + flugnummer + '\'' +
+                ", airline='" + airline + '\'' +
+                ", von='" + von + '\'' +
+                ", nach='" + nach + '\'' +
+                ", flugdatum=" + flugdatum +
+                ", uhrzeit=" + uhrzeit +
+                '}';
+    }
 }
--- ui-service/src/main/java/com/github/cosmoem/uiservice/handlingformsubmission/FlugController.java
+++ ui-service/src/main/java/com/github/cosmoem/uiservice/handlingformsubmission/FlugController.java
@@ -1,13 +1,17 @@
 package com.github.cosmoem.uiservice.handlingformsubmission;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class FlugController {
+
+    private RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/flug")
     public String flugForm(Model model) {
@@ -17,6 +21,12 @@ public class FlugController {
 
     @PostMapping("/flug")
     public String flugSubmit(@ModelAttribute Flug flug) {
+        Flug res = restTemplate.getForObject("http://localhost:8083/api/flug/{flugnummer}", Flug.class, flug.getFlugnummer());
+        flug.setAirline(res.getAirline());
+        flug.setFlugdatum(res.getFlugdatum());
+        flug.setNach(res.getNach());
+        flug.setVon(res.getVon());
+        flug.setUhrzeit(res.getUhrzeit());
         return "result";
     }
 
