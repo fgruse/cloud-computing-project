fdb9934b2313c63055b0ddd5a7602a09e05b9a9c
Anna-Frieda Gruse <anna-frieda.gruse@zalando.de>
2020-02-11 13:53:42 +0100

    update db-service

--- db-service/src/main/java/com/github/cosmoem/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/github/cosmoem/dbservice/DbServiceApplication.java
@@ -1,10 +1,12 @@
 package com.github.cosmoem.dbservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DbServiceApplication {
 
