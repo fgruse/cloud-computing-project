e4d591f79e4c04572b0f46c775b1e6a00b69164a
Anna-Frieda Gruse <anna-frieda.gruse@zalando.de>
2020-02-11 14:28:05 +0100

    update db-service

--- db-service/src/main/java/com/github/cosmoem/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/github/cosmoem/dbservice/DbServiceApplication.java
@@ -2,14 +2,15 @@ package com.github.cosmoem.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan(basePackages = {"com.github.cosmoem.dbservice.jpa.entity"})
 public class DbServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbServiceApplication.class, args);
 	}
-
 }
