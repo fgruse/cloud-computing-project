6033c1fb6725776e820ad3ef647d682234154482
Sophie Schröder <sophieschroder@Sophies-MBP.Speedport_W723_V_Typ_A_1_01_022>
2020-02-11 12:36:47 +0100

    iui

--- ui-service/src/main/java/com/github/cosmoem/uiservice/handlingformsubmission/GreetingController.java
+++ ui-service/src/main/java/com/github/cosmoem/uiservice/handlingformsubmission/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class FlightController {
+public class GreetingController {
 
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
