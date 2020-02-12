6d531505a89b87ee1a4bfe0141ede6dc87cd488e
Frieda Gruse <fr.ida@gmx.de>
2020-02-12 20:27:29 +0100

    docker compose & script to run all services

---
+++ run.sh
@@ -0,0 +1,20 @@
+# chmod u+x run.bat
+# ./run.bat
+
+cd config-server
+./gradlew clean build -x test
+docker build -t config-server .
+cd ../data-service
+./gradlew clean build -x test
+docker build -t data-service .
+cd ../db-service
+./gradlew clean build -x test
+docker build -t db-service .
+cd ../service-discovery
+./gradlew clean build -x test
+docker build -t service-discovery .
+cd ../ui-service
+./gradlew clean build -x test
+docker build -t ui-service .
+cd ..
+docker-compose -f docker-compose.yml up --remove-orphans
